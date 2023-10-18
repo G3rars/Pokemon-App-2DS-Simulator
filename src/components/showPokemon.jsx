88c5c792a6a1333c134d100aca1eafa720dc1d03
jerry <jerrygonzalezzambrano@gmail.com>
2023-10-18 23:59:36 +0200

    correcion de imagen pokemon

--- src/components/showPokemon.jsx
+++ src/components/showPokemon.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ShowPokemon({ activePokemon }) {
   return (
     <>
-      <div className="bg-gray-700 flex justify-center items-center opacity-80 absolute w-[190px] sm:w-[346px] h-[113px] sm:h-[211px] translate-y-[-60px] sm:translate-y-[-110px]">
+      <div className="bg-gray-700 flex justify-center items-center opacity-80 absolute w-[190px] sm:w-[346px] h-[113px] sm:h-[211px] translate-y-[-61px] sm:translate-y-[-110px]">
       </div>
       <div className=" flex justify-center items-center absolute w-[190px] sm:w-[346px] h-[113px] sm:h-[211px] translate-y-[-60px] sm:translate-y-[-110px]">
           {(activePokemon && activePokemon.sprites) ? (
