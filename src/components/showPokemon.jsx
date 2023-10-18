925b8a3f7d1bc4be1a6204664c123cfee43d5a12
jerry <jerrygonzalezzambrano@gmail.com>
2023-10-18 23:56:51 +0200

    correcion de imagen pokemon

--- src/components/showPokemon.jsx
+++ src/components/showPokemon.jsx
@@ -7,7 +7,7 @@ function ShowPokemon({ activePokemon }) {
       </div>
       <div className=" flex justify-center items-center absolute w-[190px] sm:w-[346px] h-[113px] sm:h-[211px] translate-y-[-60px] sm:translate-y-[-110px]">
           {(activePokemon && activePokemon.sprites) ? (
-            <img className="w-[200px]" src={activePokemon.sprites.front_default} alt="pokemonimage" />
+            <img className="w-[120px] sm:w-[200px]" src={activePokemon.sprites.front_default} alt="pokemonimage" />
           ) : (
             <p>Loading...</p>
           )}
